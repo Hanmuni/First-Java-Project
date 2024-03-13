962b0682f0cef802349157550448ea2614b20439
Hồng-Hạnh, Chu <67581292+Hanmuni@users.noreply.github.com>
2024-03-13 15:37:13 +0100

    refactor: move Person Unit Test

---
+++ src/test/Person/PersonTest.java
@@ -0,0 +1,36 @@
+package Person;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+class PersonTest {
+    Person A = new Person(28, "Eiichiro", "Oda", 1.75, 80);
+    Person B = new Person(28, "Eiichiro", "Oda", 1.75, 80);
+    Person C = new Person(35, "Tite", "Kubo", 1.70, 88);
+
+    @Test
+    void testEquals() {
+        assertEquals(A, B);
+    }
+
+    @Test
+    void testNotEquals() {
+        assertNotEquals(A, C);
+    }
+
+    @Test
+    void testHashMapKeyValue() {
+        Map<Person, String> map = new HashMap<>();
+        map.put(A, "ValueA");
+        String result1 = map.get(A);
+        String result2 = map.get(B);
+        assertEquals("ValueA", result1);
+        assertEquals("ValueA", result2);
+
+    }
+}
